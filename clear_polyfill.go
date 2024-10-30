f31846b6880f1af2817d357488decac571cfc2a1
Bartłomiej Krukowski <bkrukowski@users.noreply.github.com>
2024-10-30 22:11:26 +0400

    chore: `clear` -> `clearSlice` (#10)

--- clear_polyfill.go
+++ clear_polyfill.go
@@ -2,7 +2,7 @@ package slice
 
 // The function [clear] has been introduced in GO 1.21.
 // See https://tip.golang.org/doc/go1.21#language.
-func clear[S ~[]T, T any](in S) {
+func clearSlice[S ~[]T, T any](in S) {
 	var x T
 
 	for i := 0; i < len(in); i++ {
